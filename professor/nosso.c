0240e6cc9769a263163008d9c10eb103d1cbfe72
Schatzmann <annelyse94@gmail.com>
2018-06-23 02:48:58 -0300

    Novo arquivo de configuração do servidor redis e novo jogador

--- professor/nosso.c
+++ professor/nosso.c
@@ -4,7 +4,7 @@
 #include "campo.h"
 
 #define MAXSTR 512
-// #define MAXINT 16
+#define MAXINT 16
 
 int main(int argc, char **argv) {
   char buf[MAXSTR];
@@ -14,53 +14,55 @@ int main(int argc, char **argv) {
   char mov_adv;
   char *pont_bola;
   char pos_saltos[MAXSTR / 2];
-  int indPosSalto;
-  int pos_meuf;
+  int vetorVazios[MAXSTR / 2];
+  char pulo[MAXSTR];
+  int pos_meuf, qtdePulos, gol_e, gol_d;
   int tam_campo;
   int pos_filo;
   int pos_bola;
   int num_saltos;
-  int i;
-  int podeSaltar;
+  int i, j, z, podePular;
+  char str[MAXINT];
 
   // conecta com o controlador do campo
   campo_conecta(argc, argv);
 
   campo_recebe(buf);
 
-  	// separa os elementos do string recebido
-  	sscanf(strtok(buf, " \n"), "%c", &lado_meu);
-  	sscanf(strtok(NULL, " \n"), "%d", &tam_campo);
-  	sscanf(strtok(NULL, " \n"), "%s", campo);
-  	sscanf(strtok(NULL, " \n"), "%c", &lado_adv);
-  	sscanf(strtok(NULL, " \n"), "%c", &mov_adv);
+  // separa os elementos do string recebido
+  sscanf(strtok(buf, " \n"), "%c", &lado_meu);
+  sscanf(strtok(NULL, " \n"), "%d", &tam_campo);
+  sscanf(strtok(NULL, " \n"), "%s", campo);
+  sscanf(strtok(NULL, " \n"), "%c", &lado_adv);
+  sscanf(strtok(NULL, " \n"), "%c", &mov_adv);
   	
- 	  if(mov_adv == 'f'){
- 	    sscanf(strtok(NULL, " \n"), "%d", &pos_filo);
- 	  }
+ 	if(mov_adv == 'f'){
+ 	  sscanf(strtok(NULL, " \n"), "%d", &pos_filo);
+ 	}
 
- 	  if(lado_meu == 'e'){
- 	  	pos_meuf = tam_campo;
- 	  } else {
- 	  	pos_meuf = 1;
- 	  }
+ 	if(lado_meu == 'e'){
+ 		pos_meuf = tam_campo;
+ 	} else {
+ 		pos_meuf = 1;
+ 	}
 
- 	  if(pos_meuf == pos_filo){
- 	  	pos_meuf = tam_campo / 2;
- 	  	pos_meuf += (lado_meu == 'e') ? 2 : 0;
- 	  }
+ 	if(pos_meuf == pos_filo){
+ 		pos_meuf = tam_campo / 2;
+ 		pos_meuf += (lado_meu == 'e') ? 2 : 0;
+ 	}
 
-  	// prepara um string com o movimento
-  	// de inserir um filosofo na posicao 1 do campo
-  	sprintf(buf, "%c f %d\n", lado_meu, pos_meuf);
+  // prepara um string com o movimento
+  // de inserir um filosofo na posicao 1 do campo
+  sprintf(buf, "%c f %d\n", lado_meu, pos_meuf);
 
-  	// envia o movimento para o controlador do campo
-  	campo_envia(buf);  
+  // envia o movimento para o controlador do campo
+  campo_envia(buf);  
 
 
   while(1) {
+  
     campo_recebe(buf);
- 
+
   	// separa os elementos do string recebido
   	sscanf(strtok(buf, " \n"), "%c", &lado_meu);
   	sscanf(strtok(NULL, " \n"), "%d", &tam_campo);
@@ -69,33 +71,79 @@ int main(int argc, char **argv) {
   	sscanf(strtok(NULL, " \n"), "%c", &mov_adv);
  
   	pont_bola = strchr(campo, 'o');
-  	indPosSalto = 0;
 
-  	pos_bola = pont_bola - campo;
+  	pos_bola = (pont_bola - campo) + 1;
 
-  	if(lado_meu == 'e'){
-  		if(campo[pos_bola + 1] == 'f'){
-  			podeSaltar = 1;
-  			for(i = pos_bola + 2; ((i <= tam_campo) && podeSaltar); i++){
- 					if(campo[i + 1] != 'f'){
- 	 					podeSaltar = 0;
- 	 					pos_saltos[indPosSalto] = (char) i;
- 	 				} else {
- 	 					pos_saltos[indPosSalto] = (char) i;
- 	 				}
- 	 				indPosSalto++;
-  			}
+  	// Verifica se pode saltar
+   	j = 0;
+   	qtdePulos = 0;
+   	gol_e = tam_campo + 1;
+   	gol_d = 0;
+
+   	if(lado_meu == 'e'){
+   		pos_meuf = pos_bola + 1;
+   		if(campo[pos_bola] == 'f'){
+   			podePular = 1;
+   			for(i = pos_bola; i < tam_campo; i++){
+   				if(campo[i] == '.'){
+						vetorVazios[j] = i;   				
+	   				j++;
+   				}
+   			}
+
+   			j++;
+   			vetorVazios[j] = gol_e;
+
+   			z = 0;
+   			while((z < j) && (campo[vetorVazios[z]] != pos_bola + 1)){
+   				qtdePulos++;
+   				sprintf(str, "%d", vetorVazios[z]);
+   				strcat(pulo, str);
+   				strcat(pulo, " ");
+   				pos_bola = vetorVazios[z];
+   				z++;
+   			}
   		}
-  	}
+
+  	} else {
+  		
+  		pos_meuf = pos_bola - 1;
+  		
+  		if(campo[pos_bola - 2] == 'f'){
+   			podePular = 1;
+   			for(i = pos_bola - 2; i > 0; i--){
+   				if(campo[i] == '.'){
+						vetorVazios[j] = i;   				
+	   				j++;
+   				}
+   			}
+
+   			j++;
+   			vetorVazios[j] = gol_d;
+
+   			z = 0;
+   			while((z < j) && (campo[vetorVazios[z]] != pos_bola - 1)){
+   				qtdePulos++;
+   				sprintf(str, "%d", vetorVazios[z]);
+   				strcat(pulo, str);
+   				strcat(pulo, " ");
+   				pos_bola = vetorVazios[z];
+   				z++;
+   			}
+  		}
+		}
+
+  	if(podePular){
+  		sprintf(buf, "%c o %s\n", lado_meu, pulo);
+		} else {
+			sprintf(buf, "%c f %d\n", lado_meu, pos_meuf);
+		}
+
+		podePular = 0;
+		strcpy(pulo, "");
 	
-
- //  	// prepara um string com o movimento
- //  	// de inserir um filosofo na posicao 1 do campo
- //  	sprintf(buf, "%c f %d\n", lado_meu, 1);
-
-
-
- //  	// envia o movimento para o controlador do campo
+  	// envia o movimento para o controlador do campo
+		printf("%s\n", buf);
   	campo_envia(buf);  
 	}
 }
